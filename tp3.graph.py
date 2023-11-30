fa72d394b45aefb47a70d0cc52d6d10a6f3ff066
marielariela <montesinos.laplata@gmail.com>
2023-11-30 17:32:51 -0300

    puntaje_en_pantalla

--- tp3.graph.py
+++ tp3.graph.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-import ppt_back as back
+import tp3_back as back
 
 #back.derrotas
 juego = back.PPT()
@@ -25,13 +25,8 @@ def elijo_tijera():
     print("Resultado",resul)
     str_resultado.set(resul)
 
-#x= 0
 
-#def tijera():
-#    global x
-#    x+=1
-#    str_resultado.set(f"Hola {x}")
-str_resultado.set("")
+#str_resultado.set("")
 
 text_resultado = ttk.Label(ventana, textvariable = str_resultado)
 text_resultado.place(x=20,y=60)
@@ -40,10 +35,9 @@ boton_piedra.place(x=20, y=160)
 boton_papel = ttk.Button(text="Papel", command=elijo_papel)
 boton_papel.place(x=100, y=160)
 boton_tijera = ttk.Button(text="Tijera", command=elijo_tijera)
-
-
-
 boton_tijera.place(x=180, y=160)
 
 
 ventana.mainloop()
+
+juego.puntaje()
--- tp3_back.py
+++ tp3_back.py
@@ -3,16 +3,20 @@ from random import choice
 class PPT:
     opciones = ['piedra', 'papel', 'tijera']
 
-    ultimo_ganador = None
-    txt = ""
+    #ultimo_ganador = None
+    #txt = ""
+    puntaje_us=0
+    puntaje_pc=0
 
     def pc_juega(self):
         return choice(PPT.opciones)
     
     def ganador_player(self, player, pc):
         if (player== self.opciones[0] and pc==self.opciones[2]) or(player==self.opciones[1] and pc==self.opciones[0]) or(player==self.opciones[2] and pc==self.opciones[1]):
+            PPT.puntaje_us+=1
             return True
         else:
+            PPT.puntaje_pc+=1
             return False
         
     def empate(self, player, pc):
@@ -20,7 +24,7 @@ class PPT:
 
     def jugar(self, mi_opcion):
         jugada_pc = self.pc_juega()
-        #if self.definir_ganador(mi_opcion, jugada_pc)
+        
         if self.ganador_player(mi_opcion, jugada_pc):
             return "¡GANASTE! Felicitaciones"
         elif self.empate(mi_opcion, jugada_pc):
@@ -38,3 +42,6 @@ class PPT:
 
     def jugar_con_tijera(self):
         return self.jugar(PPT.opciones[2])
+    
+    def puntaje(self):
+        print(f"User: {self.puntaje_us}\nPC: {self.puntaje_pc}")
